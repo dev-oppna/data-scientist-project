fb528a0cefb7b2d2a02ad37e76f6be11ea51d152
dev-oppna <dev@oppna.id>
2025-05-20 11:04:33 +0700

    change secret token to env variable

--- projects/watchlist/utils.py
+++ projects/watchlist/utils.py
@@ -1,13 +1,16 @@
 import json
 import requests
+import os
 
 base_url = "https://api-stg.oppna.dev"
+basic_panthers = os.getenv("basic_panthers")
+token_panthers = os.getenv("token_panthers")
 
 def get_watchlist(name: str, phone: str, nik: str, email:str, dob:str, location:str) -> dict:
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Basic b3BwbmE6b3BwbmFwYXNzd29yZDEyMw==",
-        "Token": "6f9e4ebb7e868aa084788dd576337057afef823d3402ba5fd2fb135b6947c29d"
+        "Authorization": f"Basic {basic_panthers}",
+        "Token": {token_panthers}
     }
     data = json.dumps({
         "name" : name,
